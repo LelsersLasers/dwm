fc12c390a27b46a77cc522396d2a5a0b9675e305
LelsersLasers <48894200+LelsersLasers@users.noreply.github.com>
2020-03-24 10:39:46 -0700

    more patches

--- dwm/config.h
+++ dwm/config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 12;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -37,11 +38,13 @@ static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "grid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "Flo",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "HHH",      grid },
 };
 
 /* key definitions */
@@ -59,6 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
+static const char *googlecmd[]   = {"google-chrome-stable", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -79,15 +83,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, //make the current layout 'Master and Stack'
 	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[1]} }, //make the current layout 'Floating', note: clashes with increase number in 'Master' section
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, //make the current layout 'Max'
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} }, // make current layout 'Grid'
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },              //toggles the current to window to fullscreen
 	{ MODKEY,                       XK_space,  setlayout,      {0} },              //not quite sure, seems to switch between the last 2 layouts used
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },              //Toggles the current window to floating or not floating
+	{ MODKEY,                       XK_c,      spawn,          {.v = googlecmd} }, //start google chrome
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },      //creates a temporary WS with all windows (from all WSs)
 	//{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },      //'tags' all the WSs with the current window
 	//{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },       //Has to do with Monitors
 	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },       //Have only 1 monitor so I can't test
 	//{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },       //^
 	//{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },       //^
+	//{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	//{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	//{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
