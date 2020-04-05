48b6a11a4acac5ccc154e51998eb871a4ab98d97
LelsersLasers <48894200+LelsersLasers@users.noreply.github.com>
2020-04-05 13:11:20 -0700

    stuff for bar

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,21 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	
+	{" Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "~/dwmBar/pacupdate",						0,		9},
+
+	{"Vol: ", "~/dwmBar/volume",						0,		10},
+
+	//{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
+	
+	//{"", "~/dwmBar/battery",						5,		0},
+
+	//{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+
+	{"", "~/dwmBar/clock",							5,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
