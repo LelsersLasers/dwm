36b1d37a0f7e7731df2412119a7f21d03f619ce7
LelsersLasers <48894200+LelsersLasers@users.noreply.github.com>
2020-04-27 21:16:24 -0700

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 12;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -13,6 +13,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+//static const char col_cyan[]        = "#1c474f"; //my blue
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -33,7 +34,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -41,7 +42,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "Flo",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "HHH",      grid },
 };
@@ -60,37 +61,39 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "termite", NULL };
+static const char *googlecmd[]   = {"google-chrome-stable", NULL};
 
-#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } }, //start dmenu
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },  //start terminal (termite)
+	//{ MODKEY,                       XK_b,      togglebar,      {0} },            //toggle bar
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },       //move focus one down
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },       //move focus one up
+	//{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },     //increase number in 'Master' section
+	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },     //decrease number in 'Master' section, note: clashes with dmenu launcher
+	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },     //decrease 'Master' width
+	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },     //increase 'Master' width
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },       //move current window up instack
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },       //move current window down in stack
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },              //make the current window the 'Master' window
+	{ MODKEY,                       XK_Tab,    view,           {0} },              //switches between the last 2 used workspaces
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },              //close the current window
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, //make the current layout 'Master and Stack'
+	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[1]} }, //make the current layout 'Floating', note: clashes with increase number in 'Master' section
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, //make the current layout 'Max'
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} }, // make current layout 'Grid'
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },              //toggles the current to window to fullscreen
+	{ MODKEY,                       XK_space,  setlayout,      {0} },              //not quite sure, seems to switch between the last 2 layouts used
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },              //Toggles the current window to floating or not floating
+	{ MODKEY,                       XK_c,      spawn,          {.v = googlecmd} }, //start google chrome
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },      //creates a temporary WS with all windows (from all WSs)
+	//{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },      //'tags' all the WSs with the current window
+	//{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },       //Has to do with Monitors
+	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },       //Have only 1 monitor so I can't test
+	//{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },       //^
+	//{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },       //^
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
@@ -103,7 +106,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_x,      quit,           {0} },
 };
 
 /* button definitions */
